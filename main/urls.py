990fef83554851099835ee982ef5c2149e5355dd
Grzegorz_Wisniewski <grzechuwisniewski18@gmail.com>
2022-10-25 19:41:58 +0200

    Add empty main page

--- main/urls.py
+++ main/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include, re_path
+
+from django.urls import re_path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^', include("main.urls")),
+    re_path(r'^$', views.mainpage, name="mainpage"),
 ]
